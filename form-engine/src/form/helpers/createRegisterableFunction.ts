1b623cbad4751e1c8b51a7dc6cfed66317722399
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-08-21 18:39:29 +0100

    Update createRegisterableFunction to be a bit more explicit/better typed

--- form-engine/src/form/helpers/createRegisterableFunction.ts
+++ form-engine/src/form/helpers/createRegisterableFunction.ts
@@ -1,22 +1,22 @@
 import { FunctionExpr, ValueExpr } from '../types/expressions.type'
 
-export type FunctionEvaluator<A extends readonly ValueExpr[]> = (
+export type FunctionEvaluator<A extends readonly ValueExpr[], R = any> = (
   value: ValueExpr,
   ...args: A
-) => boolean | Promise<boolean>
+) => R | Promise<R>
 
-interface RegistryFunction<A extends readonly ValueExpr[]> {
+interface RegistryFunction<A extends readonly ValueExpr[], R = any> {
   (...args: A): FunctionExpr<A>
   spec: {
     name: string
-    evaluate: FunctionEvaluator<A>
+    evaluate: FunctionEvaluator<A, R>
   }
 }
 
-function createRegisterableFunction<A extends readonly ValueExpr[]>(
+function createRegisterableFunction<A extends readonly ValueExpr[], R = any>(
   name: string,
-  evaluator: FunctionEvaluator<A>,
-): RegistryFunction<A> {
+  evaluator: FunctionEvaluator<A, R>,
+): RegistryFunction<A, R> {
   const fn = (...args: A) =>
     ({
       type: 'function',
@@ -24,14 +24,20 @@ function createRegisterableFunction<A extends readonly ValueExpr[]>(
       arguments: args,
     }) as FunctionExpr<any>
 
-  ;(fn as RegistryFunction<A>).spec = {
+  ;(fn as RegistryFunction<A, R>).spec = {
     name,
     evaluate: evaluator,
   }
 
-  return fn as RegistryFunction<A>
+  return fn as RegistryFunction<A, R>
 }
 
-export const buildConditionFunction = createRegisterableFunction
+export const buildConditionFunction = <A extends readonly ValueExpr[]>(
+  name: string,
+  evaluator: FunctionEvaluator<A, boolean>,
+) => createRegisterableFunction<A, boolean>(name, evaluator)
 
-export const buildTransformerFunction = createRegisterableFunction
+export const buildTransformerFunction = <A extends readonly ValueExpr[]>(
+  name: string,
+  evaluator: FunctionEvaluator<A, any>,
+) => createRegisterableFunction<A, any>(name, evaluator)
