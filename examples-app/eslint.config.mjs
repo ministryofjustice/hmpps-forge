ee492304284a2873417f80d562c246e896f79000
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-04-03 19:55:53 +0100

    Change eslint rules

--- examples-app/eslint.config.mjs
+++ examples-app/eslint.config.mjs
@@ -1,3 +1,11 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
-export default hmppsConfig()
+export default [
+  ...hmppsConfig(),
+  {
+    rules: {
+      'import/prefer-default-export': 'off',
+      'import/no-named-as-default': 'off',
+    },
+  },
+]
