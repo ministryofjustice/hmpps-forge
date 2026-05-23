a99b4589f5898818efea52328eaf4f955e99a942
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-05-23 18:24:41 +0100

    Update types and delete old files

--- packages/forge-core/src/engine/types/compilationArtefacts.type.ts
+++ packages/forge-core/src/engine/types/compilationArtefacts.type.ts
@@ -1,12 +1,6 @@
 import type { NodeId } from './ast.type'
 import type { JourneyASTNode, StepASTNode } from './structures.type'
-import type { CompilationContext } from '../compilation/CompilationContext'
-import type {
-  JourneyRuntimePlan,
-  NavigationRuntimePlan,
-  ReachabilityCompilationPlan,
-  StepRuntimePlan,
-} from './runtimePlans.type'
+import type { NavigationRuntimePlan, StepRuntimePlan } from './runtimePlans.type'
 import type {
   CompiledAnswerPreparationFunction,
   CompiledEntryValidationFunction,
@@ -18,17 +12,6 @@ export type StepIndex = Map<NodeId, StepASTNode>
 
 export type JourneyIndex = Map<NodeId, JourneyASTNode>
 
-export interface SharedCompiledForm {
-  rootNode: JourneyASTNode
-  sharedContext: CompilationContext
-  stepIndex: StepIndex
-  journeyIndex: JourneyIndex
-  stepRuntimePlans: Map<NodeId, StepRuntimePlan>
-  navigationPlans: Map<NodeId, NavigationRuntimePlan>
-  reachabilityCompilationPlans: ReachabilityCompilationPlan[]
-  journeyRuntimePlans: Map<NodeId, JourneyRuntimePlan>
-}
-
 export interface CompiledStep {
   runtimePlan: StepRuntimePlan
   navigationPlan: NavigationRuntimePlan
@@ -37,7 +20,3 @@ export interface CompiledStep {
   compiledRender?: CompiledRenderFunction
   compiledAnswerPreparation: CompiledAnswerPreparationFunction | undefined
 }
-
-export type CompiledForm = CompiledStep[]
-
-export type CompilationArtefact = CompilationContext
