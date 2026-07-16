2af2b62db951ad361af93a81e5cf6177785aa038
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-07-16 15:09:03 +0100

    Lint fix for `toErrorList`

--- packages/forge-govuk-components/src/utils/toErrorList.ts
+++ packages/forge-govuk-components/src/utils/toErrorList.ts
@@ -20,14 +20,16 @@ export function getErrorSummaryList(this: NunjucksGlobalContext): ErrorListItem[
   const domainErrors = this.ctx.domainValidationErrors ?? []
   const seen = new Set<string>()
 
-  return [...domainErrors, ...fieldErrors]
-    .filter(error => {
-      const key = error.blockCode ?? error.message
+  return (
+    [...domainErrors, ...fieldErrors]
+      .filter(error => {
+        const key = error.blockCode ?? error.message
 
-      return !seen.has(key) && seen.add(key)
-    })
-    .map(error => ({
-      text: error.message,
-      href: error.blockCode ? `#${error.blockCode}` : undefined,
-    }))
+        return !seen.has(key) && seen.add(key)
+      })
+      .map(error => ({
+        text: error.message,
+        href: error.blockCode ? `#${error.blockCode}` : undefined,
+      }))
+  )
 }
