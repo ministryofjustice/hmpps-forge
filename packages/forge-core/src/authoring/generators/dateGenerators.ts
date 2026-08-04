78c41fdb747a5382a1ab609053c4213a8804d408
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-08-04 21:45:03 +0100

    Add argumentSchema's for generators

--- packages/forge-core/src/authoring/generators/dateGenerators.ts
+++ packages/forge-core/src/authoring/generators/dateGenerators.ts
@@ -1,17 +1,32 @@
+import { z } from 'zod'
 import GeneratorRegistry from '../registries/GeneratorRegistry'
+
+const noArgsSchema = z.tuple([])
 
 const dateGenerators = new GeneratorRegistry()
 
 export const DateGenerators = {
   /** Generates the current date and time */
-  Now: dateGenerators.register('Date.Now', () => () => new Date()),
+  Now: dateGenerators.register(
+    'Date.Now',
+    {
+      argumentsSchema: noArgsSchema,
+    },
+    () => () => new Date(),
+  ),
 
   /** Generates today's date at midnight */
-  Today: dateGenerators.register('Date.Today', () => () => {
-    const now = new Date()
+  Today: dateGenerators.register(
+    'Date.Today',
+    {
+      argumentsSchema: noArgsSchema,
+    },
+    () => () => {
+      const now = new Date()
 
-    return new Date(now.getFullYear(), now.getMonth(), now.getDate())
-  }),
+      return new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    },
+  ),
 }
 
 export { dateGenerators as dateGeneratorsRegistry }
