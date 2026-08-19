391ca87f0cd8a109fdca7a01f32721f733672233
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-08-19 21:39:05 +0100

    Remove deprecated function definition utilities

--- packages/forge-core/src/authoring/index.ts
+++ packages/forge-core/src/authoring/index.ts
@@ -17,19 +17,6 @@ export { default as ConditionRegistry } from './registries/ConditionRegistry'
 export { default as TransformerRegistry } from './registries/TransformerRegistry'
 export { default as GeneratorRegistry } from './registries/GeneratorRegistry'
 export { default as EffectRegistry } from './registries/EffectRegistry'
-/** @deprecated Use BaseFunctionRegistry.build() instead */
-export { createFunctionsRegistry } from './utils/deprecated/createFunctionsRegistry'
-/** @deprecated Use ConditionRegistry instead */
-export { defineConditionFunctions } from './utils/deprecated/defineConditionFunctions'
-/** @deprecated Use EffectRegistry instead */
-export { defineEffectFunctions } from './utils/deprecated/defineEffectFunctions'
-/** @deprecated Use GeneratorRegistry instead */
-export { defineGeneratorFunctions } from './utils/deprecated/defineGeneratorFunctions'
-/** @deprecated Use TransformerRegistry instead */
-export { defineTransformerFunctions } from './utils/deprecated/defineTransformerFunctions'
-/** @deprecated Use ConditionRegistry/TransformerRegistry/EffectRegistry/GeneratorRegistry inline instead */
-export { createFunctionScope, type FunctionScope } from './utils/deprecated/createFunctionScope'
-export type { FunctionImplementations, FunctionShapeMap } from './utils/deprecated/defineFunction.type'
 
 export { EffectFunctionContext } from '../engine/chassis/runtime/context/EffectFunctionContext'
 
