d14cb98a41f0cd8275b6b41908dd968c809bbf73
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-08-29 21:09:46 +0100

    Deprecate legacy function registry authoring APIs

--- packages/forge-core/src/authoring/registries/GeneratorRegistry.ts
+++ packages/forge-core/src/authoring/registries/GeneratorRegistry.ts
@@ -3,6 +3,21 @@ import type { ChainableGenerator } from '../builders/types'
 import { FunctionCallType } from '../../shared/taxonomy'
 import { BaseFunctionRegistry, type RegistrationOptions } from './BaseFunctionRegistry'
 
+/**
+ * @deprecated Use `generator()` to define each generator as a standalone entry:
+ *
+ * ```typescript
+ * const LoadCase = generator<Dependencies>('LoadCase', {
+ *   factory: dependencies => async (caseId: string) => dependencies.api.loadCase(caseId),
+ * })
+ *
+ * LoadCase(Params('caseId'))
+ * ```
+ *
+ * You don't need to register the generator or add it to the package. Just use `LoadCase()` in your
+ * journey and Forge will pick it up. If your journey is JSON and refers to the generator only by
+ * name, add it with `functions: [LoadCase]`.
+ */
 export default class GeneratorRegistry<TDeps = Record<string, never>> extends BaseFunctionRegistry<TDeps> {
   constructor() {
     super(FunctionCallType.GENERATOR)
